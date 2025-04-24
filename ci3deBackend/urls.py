084b1fdb6b99f9e2044565379962060300242429
Wallace Santana <wallacesantanak0@gmail.com>
2025-04-24 17:29:54 -0300

    (refractor): modificando a api

--- ci3deBackend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('hello/', views.hello_world),
-    path('admin/', admin.site.urls),
-]
--- ci3de_back/asgi.py
+++ ci3de_back/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for ci3deBackend project.
+ASGI config for ci3de_back project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ci3deBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ci3de_back.settings')
 
 application = get_asgi_application()
