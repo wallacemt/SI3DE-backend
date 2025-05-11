f3846a01214f49f022480974e5b0567d3154db0e
Wallace Santana <wallacesantanak0@gmail.com>
2025-05-11 15:01:55 -0300

    (feat): frontend cors liberations

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+FRONTEND_URL = os.getenv("PATH_FRONTEND")
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = "test"
 JWT_SECRET = os.getenv("JWT_SECRET")
